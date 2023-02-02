99051e0d120530fff9cdfd735f5a750f39e0cbe3
Vikysia <holy.vichka@gmail.com>
2023-02-02 23:14:29 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import bridge from '@vkontakte/vk-bridge';
+import { View, ScreenSpinner, AdaptivityProvider, AppRoot, ConfigProvider, SplitLayout, SplitCol } from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
+
+import Home from './panels/Home';
+
+
+const App = () => {
+	const [scheme, setScheme] = useState('bright_light')
+	const [activePanel, setActivePanel] = useState('home');
+	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+
+	useEffect(() => {
+		bridge.subscribe(({ detail: { type, data }}) => {
+			if (type === 'VKWebAppUpdateConfig') {
+				setScheme(data.scheme)
+			}
+		});
+
+	}, []);
+
+
+
+	return (
+		<ConfigProvider scheme={scheme}>
+			<AdaptivityProvider>
+				<AppRoot>
+					<SplitLayout popout={popout}>
+						<SplitCol>
+							<View activePanel={activePanel}>
+								<Home id='home'  />
+							</View>
+						</SplitCol>
+					</SplitLayout>
+				</AppRoot>
+			</AdaptivityProvider>
+		</ConfigProvider>
+	);
+}
+
+export default App;
---
+++ src/panels/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Panel, PanelHeader, } from '@vkontakte/vkui';
+
+const Home = ({ id }) => (
+	<Panel id={id}>
+		<PanelHeader>Example</PanelHeader>
+
+	</Panel>
+);
+
+
+export default Home;
