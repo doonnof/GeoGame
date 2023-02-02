e5f33e198026c53e207a35cf21f11248146c5f73
Vikysia <holy.vichka@gmail.com>
2023-02-03 00:06:49 +0300

    Добавит панели

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import bridge from '@vkontakte/vk-bridge';
-import { View, ScreenSpinner, AdaptivityProvider, AppRoot, ConfigProvider, SplitLayout, SplitCol } from '@vkontakte/vkui';
+import {View, AdaptivityProvider, AppRoot, ConfigProvider, SplitLayout, SplitCol} from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
 import Home from './panels/Home';
 
 
 const App = () => {
-	const [scheme, setScheme] = useState('bright_light')
-	const [activePanel, setActivePanel] = useState('home');
-	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
-
-	useEffect(() => {
-		bridge.subscribe(({ detail: { type, data }}) => {
-			if (type === 'VKWebAppUpdateConfig') {
-				setScheme(data.scheme)
-			}
-		});
-
-	}, []);
+    const [scheme, setScheme] = useState('bright_light')
+    const [activePanel, setActivePanel] = useState('home');
 
 
+    useEffect(() => {
+        bridge.subscribe(({detail: {type, data}}) => {
+            if (type === 'VKWebAppUpdateConfig') {
+                setScheme(data.scheme)
+            }
+        });
 
-	return (
-		<ConfigProvider scheme={scheme}>
-			<AdaptivityProvider>
-				<AppRoot>
-					<SplitLayout popout={popout}>
-						<SplitCol>
-							<View activePanel={activePanel}>
-								<Home id='home'  />
-							</View>
-						</SplitCol>
-					</SplitLayout>
-				</AppRoot>
-			</AdaptivityProvider>
-		</ConfigProvider>
-	);
+    }, []);
+
+
+    return (
+        <ConfigProvider scheme={scheme}>
+            <AdaptivityProvider>
+                <AppRoot>
+
+
+                    <Home id='home'/>
+
+                </AppRoot>
+            </AdaptivityProvider>
+        </ConfigProvider>
+    );
 }
 
 export default App;
--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import { Panel, PanelHeader, } from '@vkontakte/vkui';
+import {CellButton, Epic, Group, Panel, PanelHeader, View,} from '@vkontakte/vkui';
 
-const Home = ({ id }) => (
-	<Panel id={id}>
-		<PanelHeader>Example</PanelHeader>
+const Home = ({id}) => {
+    const [activePanel, setActivePanel] = useState('panel1');
+    console.log(activePanel)
 
-	</Panel>
-);
+    return <><View activePanel={activePanel}>
+        <Panel id="panel1">
+            <PanelHeader>Panel 1</PanelHeader>
+            <Group>
+                <div style={{height: 200}}/>
+                <CellButton onClick={() => setActivePanel('panel2')}>Go to panel 2</CellButton>
+                <div style={{height: 600}}/>
+            </Group>
+        </Panel>
+        <Panel id="panel2">
+            <PanelHeader>Panel 2</PanelHeader>
+            <Group>
+                <div style={{height: 200}}/>
+                <CellButton onClick={() => setActivePanel('panel3')}>Go to panel 3</CellButton>
+                <div style={{height: 600}}/>
+            </Group>
+        </Panel>
+        <Panel id="panel3">
+            <PanelHeader>Panel 3</PanelHeader>
+            <Group>
+                <div style={{height: 200}}/>
+                <CellButton onClick={() => setActivePanel('panel1')}>Back to panel 1</CellButton>
+                <div style={{height: 600}}/>
+            </Group>
+        </Panel>
+    </View>
+     {/*<Epic tabbar={}>*/}
+     {/*    <></>*/}
+     {/*</Epic>*/}
+    </>
+};
 
 
 export default Home;
